b7e776b92e8e34d28b5b26d0e3bab2b90a9cb3b5
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-06-16 12:37:40 -0300

    feat: header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: DashboardComponent
+    }
+];
