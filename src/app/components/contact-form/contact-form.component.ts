b0a2c5d77fc0b55fbda0853bc31f6f45c449fceb
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-07-29 21:46:45 -0300

    feat:  chamada da api

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,53 +1,68 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.css',
 })
 export class ContactFormComponent {
   loaded: boolean = false;
-  formContact : FormGroup;
+  formContact: FormGroup;
 
-  constructor(private forms: FormBuilder) { 
+  constructor(private forms: FormBuilder) {
     this.formContact = this.forms.group({
       from_name: new FormControl({ value: '', disabled: this.loaded }),
       from_email: new FormControl({ value: '', disabled: this.loaded }),
       subject: new FormControl({ value: '', disabled: this.loaded }),
-      message: new FormControl({ value: '', disabled: this.loaded })
+      message: new FormControl({ value: '', disabled: this.loaded }),
     });
   }
+  async sendApi() {
+    if (this.loaded) return;
 
-  async send() {
-    if(this.loaded) return;
+    if (!this.formContact.valid) {
+      alert('Preencha todos os campos');
+      return;
+    }
+
     this.loaded = true;
-    if(process.env['PUBLIC_KEY_EMAILJS'] === undefined) return;
-   emailjs.init(process.env['PUBLIC_KEY_EMAILJS']);
-   let response = await emailjs.send("service_z9vita9","template_0652ea8",{
-      from_name: this.formContact.value.from_name,
-      to_name: "to_ name",
-      from_email: this.formContact.value.from_email,
-      subject: this.formContact.value.subject,
-      message: this.formContact.value.message,
-      });
-      this.loaded = false;
-    if(response.status === 200){
-      alert('Email enviado com sucesso!!');
+    // const apiUrl = 'https://api-gdgios.vercel.app';
+    const apiUrl = 'https://api-gdgios.vercel.app';
+
+    let response = await fetch(`${apiUrl}/api/send-email`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        from_name: this.formContact.value.from_name,
+        from_email: this.formContact.value.from_email,
+        subject: this.formContact.value.subject,
+        message: this.formContact.value.message,
+        reply_to: this.formContact.value.from_email,
+      }),
+    });
+    this.loaded = false;
+
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+
+    if (response.status === 200) {
       this.formContact.reset();
     }
-    else{
-      alert('Erro ao enviar email, tente novamente.');
-    }
-    
 
+    alert(responseJson.message);
   }
-
 }
